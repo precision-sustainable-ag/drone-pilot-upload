fe89df4203be245dc76a4d29664ded1a99f45f20
Jinam Shah <jinams47@gmail.com>
2024-09-26 15:51:12 -0400

    hpc bug fix

--- ortho_processing/cron.py
+++ ortho_processing/cron.py
@@ -22,9 +22,8 @@ def generateLsfScript(flight_dir, flight_id, process_name, ortho_file=None):
             file.write(f"""#!/bin/bash
         #BSUB -n 32
         ## requested job run time
-        #BSUB -W 30:00
+        #BSUB -W 15:00
         #BSUB -q gpu
-        #BSUB -x
         #BSUB -R "select[ a100 || a10 || a30 ]"
         #BSUB -gpu "num=1:mode=shared:mps=no"
         ## Tag general output file and std error output
