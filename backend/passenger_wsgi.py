f0b97f197f50bdb29c2d69c557d4df8c5b74f28a
Mikah Pinegar <mspinega@login02.hpc.ncsu.edu>
2025-07-22 10:33:22 -0400

    remove whitespace

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,35 +1,16 @@
 # passenger_wsgi.py
-
 import sys
-
 import os
 
-
-
 # Path to the venv created with Python 3.9
-
 venv_path = "/usr/local/usrapps/drones/mspinega/drone-pilot-upload/backend/venv"
-
 site_packages = os.path.join(venv_path, "lib/python3.9/site-packages")
 
-
-
 # Update Python path and env
-
 sys.path.insert(0, site_packages)
-
 sys.path.insert(0, "/usr/local/usrapps/drones/mspinega/drone-pilot-upload/backend")
-
-
-
 os.environ["VIRTUAL_ENV"] = venv_path
-
 os.environ["PATH"] = os.path.join(venv_path, "bin") + ":" + os.environ["PATH"]
 
-
-
 # Import your app
-
 from app import app as application
-
-
